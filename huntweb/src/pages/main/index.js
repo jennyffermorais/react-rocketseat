df9446e801876be9c01fc22be4d4a2514a26f5c0
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-08-19 18:01:42 -0300

    listando produtos e adicionando estilos

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import api from "../../services/api";
 
-export default class Main extends Component {
+import "./style.css";
 
-    state = {
-        products: [], 
-    }
+export default class Main extends Component {
+  state = {
+    products: [],
+  };
   componentDidMount() {
     this.loadProducts();
   }
@@ -13,16 +14,21 @@ export default class Main extends Component {
   loadProducts = async () => {
     const response = await api.get("/products");
 
-   this.setState({ products: response.data.docs }) 
+    this.setState({ products: response.data.docs });
   };
 
   render() {
-  return (
+    const { products } = this.state;
+    return (
       <div className="product-list">
-          {this.state.products.map(product => (
-            <h2 key={product._id}>{product.title}</h2>  
-          ))}
+        {products.map((product) => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+            <a href="">Acessar</a>
+          </article>
+        ))}
       </div>
-  )
+    );
   }
 }
