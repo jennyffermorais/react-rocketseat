bb45911884bce34ab5592886ccaa94536804e28a
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-08-19 16:42:53 -0300

    buscando produtos da api

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Header from "./componentes/Header";
 
-import './styles.css'
+import "./styles.css";
+
+import Header from "./componentes/Header";
+import Main from './pages/main'
 
 const App = () => (
   <div className="App">
     <Header />
+    <Main />
   </div>
 );
 
