2d287fea560e84ce3de8189203cf306af79bf476
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-08-19 20:03:27 -0300

    configurando a navegação

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
 
 import "./style.css";
 
@@ -48,12 +49,16 @@ export default class Main extends Component {
           <article key={product._id}>
             <strong>{product.title}</strong>
             <p>{product.description}</p>
-            <a href="">Acessar</a>
+            <Link to={`/products/${product._id}`}>Acessar</Link>
           </article>
         ))}
         <div className="actions">
-          <button disabled={page == 1} onClick={this.prevPage}>Anterior</button>
-          <button disabled={page == productInfo.pages} onClick={this.nextPage}>Próxima</button>
+          <button disabled={page == 1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button disabled={page == productInfo.pages} onClick={this.nextPage}>
+            Próxima
+          </button>
         </div>
       </div>
     );
